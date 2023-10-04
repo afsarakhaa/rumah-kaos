e953956e7224dac97f827d5a076de6519e962537
afsarakhaa <afsarrakha140604@gmail.com>
2023-10-04 10:20:50 +0700

    Selesai Tugas 5

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register, login_user, logout_user
 from . import views
+from main.views import edit_product
 
 app_name = 'main'
 
@@ -18,5 +19,6 @@ urlpatterns = [
     path('tambah_amount/<int:product_id>/', views.tambah_amount, name='tambah_amount'),
     path('kurang_amount/<int:product_id>/', views.kurang_amount, name='kurang_amount'),
     path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
 
 ]
